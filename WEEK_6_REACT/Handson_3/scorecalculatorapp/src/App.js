dbba568a59c1d12f60785d0ffd85355099a8b9da
aryanxfr <aryangupta130104@gmail.com>
2025-07-26 16:28:55 +0530

    Score Calculator App

---
+++ WEEK_6_REACT/Handson_3/scorecalculatorapp/src/App.js
@@ -0,0 +1,14 @@
+import { CalculateScore } from "./components/CalculatorScore";
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name={"Steve"}
+      School={"DAV"}
+      total={284}
+      goal={3}/>
+    </div>
+  );
+}
+
+export default App;
